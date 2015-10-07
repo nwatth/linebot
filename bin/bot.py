db23d605979e57dafc2b3106fd1d72d695dbf4b0
Nantiwat Thongseng <nwatth@gmail.com>
2015-10-07 22:46:53 +0700

    change some code for test

--- bin/bot.py
+++ bin/bot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os; os.sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from line import LineClient
 from os import environ
 import re
@@ -6,22 +7,18 @@ import re
 try:
     LINE_USER_TOKEN    = environ.get("LINE_USER_TOKEN", "")
     LINE_COMPUTER_NAME = environ.get("LINE_COMPUTER_NAME", "LineBotWorker")
-    client = LineClient(authToken=LINE_USER_TOKEN, com_name=LINE_USER_TOKEN)
-    client.profile.sendMessage("Bot started..")
-except:
-    print("Login Failed")
+    client = LineClient(authToken=LINE_USER_TOKEN, com_name=LINE_COMPUTER_NAME)
+except Exception as e:
+    print "Login Failed\n", e.message
     exit()
 
 while True:
-    for group in list(set(client.groups) | set(client.contacts)):
-        chat = client.getMessageBox(group.id)
-        if not chat.unreadCount:
-            continue
-        messages = client.getRecentMessages(chat, chat.unreadCount)
-        messages.reverse()
-        for message in messages:
-            if client.profile == message.sender:
-                continue
-            elif re.match(r'(เล้ง|เลิ้ง|เลี้ยง)', message.text):
-                chat.sendMessage("ว่าไงครัซซ")
+    group = client.getGroupByName("LineBotWorker")
+    chat = client.getMessageBox(group.id)
+    messages = client.getRecentMessages(chat, chat.unreadCount)
+    messages.reverse()
+    for message in messages:
+        if re.match(r'(เล้ง|เลิ้ง|เลี้ยง)', message.text):
+            group.sendMessage("ว่าไงครัซซ")
+            break
 
